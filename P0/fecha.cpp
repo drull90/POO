bc928f512376410d9669bd765779f7792bc5c4cf
drull90 <alejandro.cantos.lopezgc@gmail.com>
2019-03-10 23:43:22 +0100

    Sobrecargando operadores

--- P0/fecha.cpp
+++ P0/fecha.cpp
@@ -18,6 +18,9 @@ Fecha::Fecha(int dia, int mes, int anno) : dia_{dia}, mes_{mes}, anno_{anno} {
  */
 Fecha::Fecha(const Fecha& fecha) : dia_{fecha.dia_}, mes_{fecha.mes_}, anno_{fecha.anno_}{}
 
+/**
+ * Constructor de parametro cadena
+ */
 Fecha::Fecha(const char* fecha) {
     if(sscanf(fecha, "%i/%i/%i", &dia_, &mes_ ,&anno_) != 3) throw Invalida();
     normalizarFecha();
@@ -44,16 +47,49 @@ bool Fecha::fechaValida() {
     time_t tiempo_calendario = time(nullptr);
     tm* fechaHoy = localtime(&tiempo_calendario);
 
-    fechaHoy->tm_year = anno_ - 1900;
-    fechaHoy->tm_mon = mes_ - 1;
     fechaHoy->tm_mday = dia_;
+    fechaHoy->tm_mon = mes_ - 1;
+    fechaHoy->tm_year = anno_ - 1900;   
 
     mktime(fechaHoy);
 
-    if(anno_ != fechaHoy->tm_year + 1900 || mes_ != fechaHoy->tm_mon + 1 || dia_ != fechaHoy->tm_mday)
+    if(anno_ != fechaHoy->tm_year + 1900 || mes_ != fechaHoy->tm_mon + 1 || dia_ != fechaHoy->tm_mday || anno_ < AnnoMinimo || anno_ > AnnoMaximo)
         return false;
 
     return true;
+}
+
+Fecha Fecha::operator++(int dia){
+    
+    Fecha aux {*this};
+
+    aux = aux + 1;
+
+    return aux;
+}
+
+Fecha Fecha::operator++(){
+
+    Fecha aux {*this};
+
+    aux = aux + 1;
+
+    return aux;
+}
+
+Fecha Fecha::operator+(int dia){
+    time_t tiempo = time(nullptr);
+    tm* fechaSumada = localtime(&tiempo);
+
+    fechaSumada->tm_mday = this->dia_ + dia;
+    fechaSumada->tm_mon = this->mes_ - 1;
+    fechaSumada->tm_year = this->anno_ - 1900;
+
+    mktime(fechaSumada);
+
+    Fecha fechaAux {fechaSumada->tm_mday, fechaSumada->tm_mon + 1, fechaSumada->tm_year  + 1900};
+
+    return fechaAux;
 }
 
 int Fecha::dia() const{
@@ -69,7 +105,18 @@ int Fecha::anno() const{
 }
 
 int main() {
-    Fecha f {"0/0/0"};
+    Fecha f{1,1,2020};
+
+    Fecha a = f;
+
     cout << f.dia() << "/" << f.mes() << "/" << f.anno() << endl;
+
+    cout << a.dia() << "/" << a.mes() << "/" << a.anno() << endl;
+
+
+    /*for(int i = 0; i < 40; ++i){
+        cout << f.dia() << "/" << f.mes() << "/" << f.anno() << endl;
+        f++;
+    }*/
     return 0;
 }
