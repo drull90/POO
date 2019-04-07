8e5d27dbde10a7beea5e316cd600613fc5bc8eb6
drull90 <alejandro.cantos.lopezgc@gmail.com>
2019-04-07 20:07:53 +0200

    P0 terminada

--- P0/cadena.hpp
+++ P0/cadena.hpp
@@ -43,23 +43,23 @@ Cadena 	operator	+	(const Cadena&, const Cadena&) 	noexcept;
 //Comparacion
 // Como el compilador no sabe que metodo usar al pasar const char* y Cadena, ya que puede transformar
 // Cadena a const char* o const char* a cadena, hay que sobrecargar 2 veces mas los operadores para dejarlo claro
-bool 	operator	<	(const Cadena&, const char*)	noexcept;
-bool 	operator	>	(const Cadena&, const char*)	noexcept;
-bool 	operator	<=	(const Cadena&, const char*) 	noexcept;
-bool 	operator	>=	(const Cadena&, const char*) 	noexcept;
-bool 	operator	!=	(const Cadena&, const char*) 	noexcept;
-bool 	operator	==	(const Cadena&, const char*) 	noexcept;
-bool 	operator	>	(const Cadena&, const Cadena&)	noexcept;
-bool 	operator 	<	(const Cadena&, const Cadena&)	noexcept;
-bool 	operator	<=	(const Cadena&, const Cadena&) 	noexcept;
-bool 	operator	>=	(const Cadena&, const Cadena&) 	noexcept;
-bool 	operator	==	(const Cadena&, const Cadena&) 	noexcept;
-bool 	operator	!=	(const Cadena&, const Cadena&) 	noexcept;
-bool 	operator	<	(const char*,  	const Cadena&)	noexcept;
-bool 	operator	>	(const char*, 	const Cadena&)	noexcept;
-bool 	operator	<=	(const char*, 	const Cadena&) 	noexcept;
-bool 	operator	>=	(const char*, 	const Cadena&) 	noexcept;
-bool 	operator	!=	(const char*, 	const Cadena&) 	noexcept;
-bool 	operator	==	(const char*, 	const Cadena&) 	noexcept;
+bool operator <	 (const Cadena&, const char*)	noexcept;
+bool operator >	 (const Cadena&, const char*)	noexcept;
+bool operator <= (const Cadena&, const char*) 	noexcept;
+bool operator >= (const Cadena&, const char*) 	noexcept;
+bool operator != (const Cadena&, const char*) 	noexcept;
+bool operator == (const Cadena&, const char*) 	noexcept;
+bool operator >	 (const Cadena&, const Cadena&)	noexcept;
+bool operator <	 (const Cadena&, const Cadena&)	noexcept;
+bool operator <= (const Cadena&, const Cadena&) noexcept;
+bool operator >= (const Cadena&, const Cadena&) noexcept;
+bool operator == (const Cadena&, const Cadena&) noexcept;
+bool operator != (const Cadena&, const Cadena&) noexcept;
+bool operator <	 (const char*,   const Cadena&)	noexcept;
+bool operator >	 (const char*, 	 const Cadena&)	noexcept;
+bool operator <= (const char*, 	 const Cadena&) noexcept;
+bool operator >= (const char*, 	 const Cadena&) noexcept;
+bool operator != (const char*, 	 const Cadena&) noexcept;
+bool operator == (const char*, 	 const Cadena&) noexcept;
 
 #endif
